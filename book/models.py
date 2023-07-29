a831ecaefefeaf6d08af0c915a8d1fe909fba94e
Thiago Carvalho da Silva <thi89248@gmail.com>
2023-07-29 20:37:26 -0300

    Book app data model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length = 35)
+    author = models.CharField(max_length = 20)
+    publication_year = models.IntegerField()
+    publishing_company = models.CharField(max_length = 15)
+
+    def __str__(self):
+        return self.title
