6898c9ed0892a545b0b3d26dbabf5a74d169a14b
Thiago Carvalho da Silva <thi89248@gmail.com>
2023-07-30 20:15:22 -0300

    User entity dara model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length = 80)
+    birth = models.DateField()
+    cpf = models.CharField(max_length = 11)
+    email = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return f'name: {self.name}, email: {self.email}'
