9d1405b4789b09d70e7cd9b51ea4704cf7970ca7
Thiago Carvalho da Silva <thi89248@gmail.com>
2023-07-31 15:57:14 -0300

    loan entity data model

--- loan/models.py
+++ loan/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from book.models import Book
+from user.models import User
 
-# Create your models here.
+
+class Loan(models.Model):
+    book = models.ForeignKey(Book, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    loan_date = models.DateField()
+    return_date = models.DateField()
+
+
+    def __str__(self):
+        return f'{self.book.title} - {self.user.name}'
